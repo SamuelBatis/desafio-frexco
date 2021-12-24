9a0283b31f9833905a9a39e18d446657f6196ed3
samuel <samuel.batistela12@gmail.com>
2021-12-24 10:52:56 -0300

    metodos get post update e delete criados

--- Back-end/server.ts
+++ Back-end/server.ts
@@ -15,20 +15,59 @@ db.on("error", () => { console.log('houve um erro') })
 db.once("open", () => { console.log('Banco Carregado') })
 
 
+
 app.use(cors())
 
 app.use(express.json())
+
+//retornar frutas
 
 app.get('/all', (req, res) => {
     return res.json(data)
 })
 
-app.post('/all', (req, res) => {
-    let fruit = new Fruit({
-        
-    })
+//retornar uma fruta
+app.get('/all/:index', (req, res) => {
+    const { index } = req.params;
+
+    return res.json(data[index])
 })
 
+// criar nova fruta
+
+app.post('/all', (req, res) => {
+    const fruit = req.body;
+
+    data.push(fruit)
+    return res.json(data)
+   
+})
+
+interface fruitsSchema {
+    genus: string;
+    name: string;
+    id: number;
+    stock: number;
+}
+
+// atualizar fruta
+app.put('/all/:index', (req, res) => {
+    const { index } = req.params;
+    const { name }: fruitsSchema = req.body;
+
+    data[index] = name
+
+    return res.json(data);
+})
+
+// Deletar fruta
+app.delete('/all/:index', (req, res) => {
+    const { index } = req.params;
+
+
+    data.splice(index, 1);
+    return res.json({message: "Fruta deletada"})
+})
 
 app.listen('3333', () => {
     console.log('Server running')
